ff0327497275106fe6986b018158e6655d231d26
Henry Shen <hk.shen94@gmail.com>
2019-11-14 20:55:18 +0100

    done

--- index.js
+++ index.js
@@ -14,6 +14,38 @@
 
 const service = (score) => {
   // code goes here
+  let arr = score.split(':')
+  let res='';
+  let num1 = Number(arr[0]);
+  let num2 = Number(arr[1]);
+  let sum = num1 +num2;
+  console.log(sum);
+  if ((num1 > 20 || num2> 20) && Math.abs(num1-num2) > 1) {
+      res="finished"
+      return res;
+  }
+
+  if (num1 > 20 && num2> 20) {
+    let a = Math.floor((sum-40)/2);
+    let b = a%2;
+    if (b === 0) {
+      res='first';
+    }
+    else res='second';
+    return res;
+  }
+  else {
+    let a = Math.floor(sum/5);
+    let b = a%2;
+    
+    if (b === 0) {
+      res='first';
+    }
+    else res='second';
+    return res;
+  }
 };
+
+// service('5:4');
 
 module.exports = service;
